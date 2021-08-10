9b68e435e6424fb4cba90d5b1c7b7d57ddfc131b
margoroma <margoroma2010@gmail.com>
2021-08-10 16:47:29 +0300

    feat: add function to checking brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config = [];
+  for (i = 0; i < bracketsConfig.length; i ++) {
+    config.push(bracketsConfig[i].join(''));
+  }
+  
+  for (i = 0; i < config.length; i ++) {
+    if (str.includes(config[i])) {
+      str = str.replace(config[i],'');
+      i = - 1;
+    }
+  }
+  return str.length === 0;
 }
